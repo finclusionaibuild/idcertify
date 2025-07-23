3f00158732ac667c1a781eb7ac555cac072a1f89
finclusionaibuild <finclusionAIbuild@gmail.com>
2025-07-23 20:44:42 +0100

    Fix syntax error in AdminBackupRecovery.tsx

--- src/modules/admin/AdminBackupRecovery.tsx
+++ src/modules/admin/AdminBackupRecovery.tsx
@@ -804,7 +804,7 @@ const AdminBackupRecovery: React.FC = () => {
 
               <div className="flex justify-end gap-3 pt-4 border-t">
                 <button
-                  onClick={() => setShow RestoreModal(false)}
+                  onClick={() => setShowRestoreModal(false)}
                   className="px-4 py-2 text-gray-600 border border-gray-300 rounded-lg hover:bg-gray-50"
                 >
                   Cancel
