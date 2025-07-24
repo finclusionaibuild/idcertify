d3e6ca4f1afc908b6a4a128ee4f2bbbcabafbf84
finclusionaibuild <finclusionAIbuild@gmail.com>
2025-07-24 01:59:06 +0100

    Updated IndividualDashboard.tsx

--- src/modules/user/IndividualDashboard.tsx
+++ src/modules/user/IndividualDashboard.tsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { useAuth } from '../shared/contexts/AuthContext'
 import PersonIcon from '@mui/icons-material/Person';
+import SecurityIcon from '@mui/icons-material/Security';
 import DescriptionIcon from '@mui/icons-material/Description';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SettingsIcon from '@mui/icons-material/Settings';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import AddIcon from '@mui/icons-material/Add';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import WarningIcon from '@mui/icons-material/Warning';
 import {
   XCircle,
   AlertCircle,
